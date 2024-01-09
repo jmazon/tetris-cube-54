df2a805ca8bbf883dc6d9c4506ab801d3c344e9b
Jean-Baptiste Mazon <jb@darkstar>
2024-01-09 16:44:49 +0100

    LTS-22.4; rewrite (not sure when it last worked)

---
+++ tetris-cube.hs
@@ -0,0 +1,53 @@
+import           Control.Lens    (view,over)
+import           Control.Monad   (guard)
+import           Data.Ix         (range,inRange)
+import           Data.List       (foldl',nub,sort)
+import           Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import           Data.Monoid     (Endo(..))
+import           Data.Set        (Set)
+import qualified Data.Set as Set
+import           Linear.Matrix   (M33,(!*),identity)
+import           Linear.V3
+import           System.Clock    (Clock(Monotonic),getTime,diffTimeSpec)
+
+type V = V3 Int
+type Shape = [V]
+
+shapes :: [Shape]
+shapes = nub $ map (normalize . flip map tee . (!*)) rots where
+  tee = [V3 0 0 0,V3 1 0 0,V3 2 0 0,V3 1 1 0]
+  -- we don't bother filtering for positive transformations because
+  -- the tee shape is invariant under inversion.
+  rots = flip appEndo identity . mconcat . map Endo <$> sequence
+    [ view <$> [_xyz,_xzy,_yxz,_yzx,_zxy,_zyx]
+    , [id, over _x negate]
+    , [id, over _y negate]
+    , [id, over _z negate]
+    ]
+  normalize s =
+    let s'@(m:_) = sort s
+    in map (subtract m) s'
+
+solve :: [Map V Int]
+solve = go Map.empty 0 (range bds) where
+  bds = (V3 0 0 0,V3 5 5 5)
+  go m _ [] = pure m
+  go m i (v:vs)
+    | v `Map.member` m = go m i vs
+    | otherwise = do
+        s <- map (v +) <$> shapes
+        guard (all (inRange bds) s)
+        guard (all (`Map.notMember` m) s)
+        let m' = foldl' (\m_ v -> Map.insert v i m_) m s
+        go m' (i+1) vs
+
+main :: IO ()
+main = do
+  start <- getTime Monotonic
+  print $ head solve
+  t1 <- getTime Monotonic
+  print $ diffTimeSpec t1 start -- 79s
+  print $ length solve
+  t2 <- getTime Monotonic
+  print $ diffTimeSpec t2 t1
